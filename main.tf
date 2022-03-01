47d21879979ac7b8e47acf81754e45e8867803f0
EC2 Default User <ec2-user@ip-172-31-95-40.ec2.internal>
2022-03-01 12:58:12 +0000

    Adding main terraform file

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_kms_key" "mykey" {
+   description             = "This key is used to encrypt bucket objects"
+   deletion_window_in_days = 10
+   tags = {
+    Name        = "My KMS Key"
+    Environment = "Sandbox"
+    Owner       = "VENKATRAMAN E K"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "raman-tf"
+
+  tags = {
+    Name        = "My S3 test bucket 22 May 2020"
+    Environment = "POC"
+    Owner       = "VENKATRAMAN E K",
+    DataType    = "Test files"
+  }
+}
+
+resource "aws_s3_bucket_acl" "b" {
+  bucket = aws_s3_bucket.b.id
+  acl = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "b" {
+  bucket = aws_s3_bucket.b.id
+  target_bucket = "raman-tf-01-Mar-2022"
+  target_prefix = "s3logs/us-east-1/"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "b" {
+  bucket = aws_s3_bucket.b.id
+  rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.mykey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+}
